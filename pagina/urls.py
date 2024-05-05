83b0440cc27a3ec2860e7357072d76c9800fa9e9
AliciaV2002 <113649609+AliciaV2002@users.noreply.github.com>
2024-05-04 23:20:59 -0500

    Se creo inicio y registrar
    se añadieron nuevos archivos html

--- pagina/urls.py
+++ pagina/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)	
+    path('', views.index),	
+    path('registrarse/', views.register),
+    path('iniciarsesion/', views.login),
+    path('cambiarcontraseña/', views.changepass)
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'registrarse.html')
+
+def login(request):
+    return render(request, 'iniciarsesion.html')
+
+def changepass(request):
+    return render(request, 'cambiarcontraseña.html')
