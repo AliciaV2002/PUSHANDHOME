cdcf6f3f31af9a7703268d1eec48087fed77b821
AliciaV2002 <113649609+AliciaV2002@users.noreply.github.com>
2024-06-03 22:58:27 -0500

    Update settings.py

--- PushandHome/settings.py
+++ PushandHome/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-zp7i3h*a_bzeg(6&%0-j16f1@$&188y$k0-ng$pq-21*@r&=$k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
